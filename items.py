5155c83b878658f64e68a9230739b8480b6d5f38
Bouthaina-hb <72989327+Bouthaina-hb@users.noreply.github.com>
2021-04-05 22:55:55 +0200

    Add files via upload

---
+++ items.py
@@ -0,0 +1,13 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+# Extracted data -> Temporary containers (items) -> Storing in database
+import scrapy
+
+
+class LipnItem(scrapy.Item):
+    # define the fields for your item here like:
+    url = scrapy.Field()
+    start_url = scrapy.Field()
+
